28d631a6c55d8ed9c5fc802364e81bf9312708e6
Airma01 <ermivandijk@gmail.com>
2026-06-13 08:03:54 +0300

    intitialConfig2

--- HospitalSys/obj/Debug/net10.0/HospitalSys.AssemblyInfo.cs
+++ HospitalSys/obj/Debug/net10.0/HospitalSys.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalSys")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08b4020724233422f0c82b5842c4ec44bb52b436")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalSys")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalSys")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
